233fe2480080aef07b2dd792d55c7ecb3e70a8c4
Antonio Gomes <a1.gomes@sisa.samsung.com>
2013-01-24 11:23:07 -0400

    2013-01-24  Antonio Gomes  <a1.gomes@sisa.samsung.com>
            Remove all "Not visible" info types from WebCLDevice and friends
            Issue #89
    
            Reviewed by Igor.
    
            Enum items and all their references being removed here are the ones marked as "Not Visibiel"
            on https://cvs.khronos.org/svn/repos/registry/trunk/public/webcl/spec/latest/index.html#5
            (5 Differences between WebCL and OpenCL 1.1).
    
            * Modules/webcl/WebCL.h:
            * Modules/webcl/WebCL.idl:
            * Modules/webcl/WebCLDevice.cpp:
            (WebCore::WebCLDevice::getInfo):

--- Source/WebCore/Modules/webcl/WebCLDevice.cpp
+++ Source/WebCore/Modules/webcl/WebCLDevice.cpp
@@ -60,9 +60,7 @@ WebCLGetInfo WebCLDevice::getInfo(int infoType, ExceptionCode& ec)
     cl_ulong ulong_units = 0;
     cl_bool bool_units = false;
     cl_device_type type = 0;
-    cl_device_mem_cache_type momoryCacheType = 0;
     cl_command_queue_properties queueProperties = 0;
-    cl_device_exec_capabilities exec = 0;
     cl_device_local_mem_type localMemoryType = 0;
     cl_device_fp_config fpConfig = 0;
     // cl_platform_id platformID;
@@ -75,10 +73,7 @@ WebCLGetInfo WebCLDevice::getInfo(int infoType, ExceptionCode& ec)
 
     switch (infoType) {
     case WebCL::DEVICE_EXTENSIONS:
-    case WebCL::DEVICE_NAME:
     case WebCL::DEVICE_PROFILE:
-    case WebCL::DEVICE_VENDOR:
-    case WebCL::DEVICE_VERSION:
     case WebCL::DEVICE_OPENCL_C_VERSION:
     case WebCL::DRIVER_VERSION:
         err = ComputeContext::getDeviceInfo(m_cl_device_id, infoType, sizeof(deviceString), &deviceString);
@@ -87,15 +82,10 @@ WebCLGetInfo WebCLDevice::getInfo(int infoType, ExceptionCode& ec)
         break;
     case WebCL::MEM_READ_ONLY:
     case WebCL::DEVICE_ADDRESS_BITS:
-    case WebCL::DEVICE_GLOBAL_MEM_CACHELINE_SIZE:
-    case WebCL::DEVICE_MAX_CLOCK_FREQUENCY:
     case WebCL::DEVICE_MAX_CONSTANT_ARGS:
     case WebCL::DEVICE_MAX_READ_IMAGE_ARGS: // unsigned int
     case WebCL::DEVICE_MAX_WRITE_IMAGE_ARGS: // unsigned int
     case WebCL::DEVICE_MAX_SAMPLERS:
-    case WebCL::DEVICE_MEM_BASE_ADDR_ALIGN:
-    case WebCL::DEVICE_MIN_DATA_TYPE_ALIGN_SIZE:
-    case WebCL::DEVICE_VENDOR_ID:
     case WebCL::DEVICE_PREFERRED_VECTOR_WIDTH_CHAR:
     case WebCL::DEVICE_PREFERRED_VECTOR_WIDTH_SHORT:
     case WebCL::DEVICE_PREFERRED_VECTOR_WIDTH_INT:
@@ -103,14 +93,6 @@ WebCLGetInfo WebCLDevice::getInfo(int infoType, ExceptionCode& ec)
     case WebCL::DEVICE_PREFERRED_VECTOR_WIDTH_FLOAT:
     case WebCL::DEVICE_PREFERRED_VECTOR_WIDTH_DOUBLE:
     case WebCL::DEVICE_PREFERRED_VECTOR_WIDTH_HALF:
-    case WebCL::DEVICE_NATIVE_VECTOR_WIDTH_SHORT:
-    case WebCL::DEVICE_NATIVE_VECTOR_WIDTH_CHAR:
-    case WebCL::DEVICE_NATIVE_VECTOR_WIDTH_INT:
-    case WebCL::DEVICE_NATIVE_VECTOR_WIDTH_LONG:
-    case WebCL::DEVICE_NATIVE_VECTOR_WIDTH_FLOAT:
-    case WebCL::DEVICE_NATIVE_VECTOR_WIDTH_DOUBLE:
-    case WebCL::DEVICE_NATIVE_VECTOR_WIDTH_HALF:
-    case WebCL::DEVICE_MAX_COMPUTE_UNITS:
         err = ComputeContext::getDeviceInfo(m_cl_device_id, infoType, sizeof(cl_uint), &uint_units);
         if (err == CL_SUCCESS)
             return WebCLGetInfo(static_cast<unsigned>(uint_units));
@@ -119,7 +101,6 @@ WebCLGetInfo WebCLDevice::getInfo(int infoType, ExceptionCode& ec)
     case WebCL::DEVICE_IMAGE2D_MAX_WIDTH:
     case WebCL::DEVICE_MAX_PARAMETER_SIZE:
     case WebCL::DEVICE_MAX_WORK_GROUP_SIZE:
-    case WebCL::DEVICE_PROFILING_TIMER_RESOLUTION: // cl_bool
     case WebCL::DEVICE_MAX_WORK_ITEM_DIMENSIONS: // unsigned int
         err = ComputeContext::getDeviceInfo(m_cl_device_id, infoType, sizeof(size_t), &sizet_units);
         if (err == ComputeContext::SUCCESS)
@@ -143,16 +124,12 @@ WebCLGetInfo WebCLDevice::getInfo(int infoType, ExceptionCode& ec)
     case WebCL::DEVICE_LOCAL_MEM_SIZE:
     case WebCL::DEVICE_MAX_CONSTANT_BUFFER_SIZE:
     case WebCL::DEVICE_MAX_MEM_ALLOC_SIZE: // unsigned long long
-    case WebCL::DEVICE_GLOBAL_MEM_CACHE_SIZE:
-    case WebCL::DEVICE_GLOBAL_MEM_SIZE:
         err = ComputeContext::getDeviceInfo(m_cl_device_id, infoType, sizeof(cl_ulong), &ulong_units);
         if (err == ComputeContext::SUCCESS)
             return WebCLGetInfo(static_cast<unsigned long>(ulong_units));
         break;
     case WebCL::DEVICE_AVAILABLE:
-    case WebCL::DEVICE_COMPILER_AVAILABLE:
     case WebCL::DEVICE_ENDIAN_LITTLE:
-    case WebCL::DEVICE_ERROR_CORRECTION_SUPPORT:
     case WebCL::DEVICE_HOST_UNIFIED_MEMORY:
     case WebCL::DEVICE_IMAGE_SUPPORT:
         err = ComputeContext::getDeviceInfo(m_cl_device_id, infoType, sizeof(cl_bool), &bool_units);
@@ -169,7 +146,6 @@ WebCLGetInfo WebCLDevice::getInfo(int infoType, ExceptionCode& ec)
         if (err == ComputeContext::SUCCESS)
             return WebCLGetInfo(static_cast<unsigned>(queueProperties));
         break;
-    case WebCL::DEVICE_SINGLE_FP_CONFIG:
     case WebCL::DEVICE_DOUBLE_FP_CONFIG:
     case WebCL::DEVICE_HALF_FP_CONFIG:
         // Part of cl_ext.h (which isn't available in Khronos).
@@ -181,16 +157,6 @@ WebCLGetInfo WebCLDevice::getInfo(int infoType, ExceptionCode& ec)
         err = ComputeContext::getDeviceInfo(m_cl_device_id, infoType, sizeof(platformID), &platformID);
         if (err == ComputeContext::SUCCESS)
             return WebCLGetInfo(String(platformID));
-        break;
-    case WebCL::DEVICE_EXECUTION_CAPABILITIES:
-        err = ComputeContext::getDeviceInfo(m_cl_device_id, infoType, sizeof(cl_device_exec_capabilities), &exec);
-        if (err == ComputeContext::SUCCESS)
-            return WebCLGetInfo(static_cast<unsigned>(exec));
-        break;
-    case WebCL::DEVICE_GLOBAL_MEM_CACHE_TYPE:
-        err = ComputeContext::getDeviceInfo(m_cl_device_id, infoType, sizeof(cl_device_mem_cache_type), &momoryCacheType);
-        if (err == ComputeContext::SUCCESS)
-            return WebCLGetInfo(static_cast<unsigned>(momoryCacheType));
         break;
     case WebCL::DEVICE_LOCAL_MEM_TYPE:
         err = ComputeContext::getDeviceInfo(m_cl_device_id, infoType, sizeof(cl_device_local_mem_type), &localMemoryType);
