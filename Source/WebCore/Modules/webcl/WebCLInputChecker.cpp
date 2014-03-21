mcgrawp/webkit-webcl

commit 061b721549e1ce23c286472a3242366db50498af

2014-02-20  Sharath Kamath  <s.kamath@samsung.com>
        [Part -1 ] Add validation for pitchValues in equeue* API's

        Issue #453

        Reviewed by Antonio.

        Adding check for validating row & slice pitch values sent by user into isValidRegionForMemoryObject().
        This commit takes only the enqueue*Rect API's.

        Testing is done to validate if the read/write goes out of bound for the
        buffer/hostPtr. In that case some OpenCL drivers throw exception and some
        dont (sometimes causing crashes).

        conformance/bindingTesting/queues/cl_commandQueue_enqueueCopyBufferRect.html
        conformance/bindingTesting/queues/cl_commandQueue_enqueueReadBufferRect.html
        conformance/bindingTesting/queues/cl_commandQueue_enqueueWriteBufferRect.html
        conformance/functionalityTesting/enqueueOperations/bufferRectOperations.html

        * Modules/webcl/WebCLCommandQueue.cpp:
        (WebCore::WebCLCommandQueue::enqueueWriteBufferRectBase):
        (WebCore::WebCLCommandQueue::enqueueReadBufferRectBase):
        * Modules/webcl/WebCLInputChecker.cpp:
        (WebCore::WebCLInputChecker::isValidRegionForMemoryObject):

Source/WebCore/Modules/webcl/WebCLInputChecker.cpp
@@ -264,19 +264,36 @@ bool isValidDataSizeForArrayBufferView(unsigned long size, ArrayBufferView* arra
 
 bool isValidRegionForMemoryObject(const Vector<size_t>& origin, const Vector<size_t>& region, size_t rowPitch, size_t slicePitch, size_t length)
 {
-    // If row_pitch is 0, row_pitch is computed as region[0].
-    rowPitch = rowPitch ? rowPitch : region[0];
-
-    // If slice_pitch is 0, slice_pitch is computed as region[1] * row_pitch.
-    slicePitch = slicePitch ? slicePitch : (region[1] * rowPitch);
-
     size_t regionArea = region[0] * region[1] * region[2];
     if (!regionArea)
         return false;
 
+    if (rowPitch) {
+        // Validate User given rowPitch, region read = rowPitch * number of rows * number of slices.
+        // The rowPitch is used to move the pointer to the next read the next row. By default its set to
+        // row width. With user sent values we must ensure the read is within the bounds.
+        size_t maximumReadPtrValue = rowPitch * region[1] * region[2];
+        if (maximumReadPtrValue > length)
+            return false;
+    }
+    if (slicePitch) {
+        // Validate User given slicePitch , region read = slicePitch * number of slices.
+        // The slicePitch is used to move the pointer for the next slice. Default value is size of slice
+        // in bytes ( region[1] * rowPitch). Must be validated identical to rowPitch to avoid out of bound memory access.
+        size_t maximumReadPtrValue = slicePitch * region[2];
+        if (maximumReadPtrValue > length)
+            return false;
+    }
+
+    // If row_pitch is 0, row_pitch is computed as region[0].
+    rowPitch = rowPitch ? rowPitch : region[0];
+    // If slice_pitch is 0, slice_pitch is computed as region[1] * row_pitch.
+    slicePitch = slicePitch ? slicePitch : (region[1] * rowPitch);
+
     // The offset in bytes is computed as origin[2] * host_slice_pitch + origin[1] * rowPitch + origin[0].
     size_t offset = origin[2] * slicePitch + origin[1]  * rowPitch + origin[0];
-    return  (regionArea + offset) <= length;
+
+    return (regionArea + offset) <= length;
 }
 
 bool isValidRegionForImage(WebCLImage* image, const Vector<CCuint>& origin, const Vector<CCuint>& region)

Source/WebCore/Modules/webcl/WebCLCommandQueue.cpp
@@ -288,8 +288,8 @@ void WebCLCommandQueue::enqueueWriteBufferRectBase(WebCLBuffer* buffer, CCbool b
     hostOriginCopy.appendVector(hostOrigin);
     regionCopy.appendVector(region);
 
-    if (!WebCLInputChecker::isValidRegionForMemoryObject(hostOriginCopy, regionCopy, hostRowPitch, hostSlicePitch, buffer->sizeInBytes())
-        || !WebCLInputChecker::isValidRegionForHostPtr(regionCopy, hostPtrLength)) {
+    if (!WebCLInputChecker::isValidRegionForMemoryObject(bufferOriginCopy, regionCopy, bufferRowPitch, bufferSlicePitch, buffer->sizeInBytes())
+        || !WebCLInputChecker::isValidRegionForMemoryObject(hostOriginCopy, regionCopy, hostRowPitch, hostSlicePitch, hostPtrLength)) {
         ec = WebCLException::INVALID_VALUE;
         return;
     }
@@ -568,7 +568,7 @@ void WebCLCommandQueue::enqueueReadBufferRectBase(WebCLBuffer* buffer, CCbool bl
     hostOriginCopy.appendVector(hostOrigin);
     regionCopy.appendVector(region);
 
-    if (!WebCLInputChecker::isValidRegionForHostPtr(regionCopy, hostPtrLength)
+    if (!WebCLInputChecker::isValidRegionForMemoryObject(hostOriginCopy, regionCopy, hostRowPitch, hostSlicePitch, hostPtrLength)
         || !WebCLInputChecker::isValidRegionForMemoryObject(bufferOriginCopy, regionCopy, bufferRowPitch, bufferSlicePitch, buffer->sizeInBytes())) {
         ec = WebCLException::INVALID_VALUE;
         return;

commit af7a4d891941a1f8a60df897a6de8f8bdfc6b2c7

2014-03-19  Sharath Kamath  <s.kamath@samsung.com>
        Moving the multiplier factor to calculation of default rowPitch.

        Regression from commit 061b721549e1ce23c286472a3242366db50498af.
        imageBytesPerPixel must be used to calculate the the default rowPitch,
        not the region accessed by the rowPitch.

        Issue #453

        Reviewed by Antonio.

        conformance/functionalityTesting/enqueueOperations/enqueue####Image.html

        * Modules/webcl/WebCLInputChecker.cpp:
        (WebCore::WebCLInputChecker::isValidRegionForHostPtr):

Source/WebCore/Modules/webcl/WebCLInputChecker.cpp
@@ -327,8 +327,8 @@ bool isValidRegionForHostPtr(const Vector<CCuint>& region, size_t rowPitch, cons
     */
     size_t imageBytesPerPixel = WebCLContext::bytesPerChannelType(descriptor->channelType())
         * WebCLContext::numberOfChannelsForChannelOrder(descriptor->channelOrder());
-    rowPitch = rowPitch ? rowPitch : region[0];
-    if (rowPitch * region[1] * imageBytesPerPixel > length)
+    rowPitch = rowPitch ? rowPitch : region[0] * imageBytesPerPixel;
+    if (rowPitch * region[1] > length)
         return false;
 
     size_t regionArea = region[0] * region[1];